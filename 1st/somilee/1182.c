f4a35b6467be1ab6b6f8837c604f1a369d531568
Somin Lee <somilee@c2r2s3.42seoul.kr>
2024-01-18 15:54:34 +0900

    1182

--- 1st/somilee/1182.c
+++ 1st/somilee/1182.c
@@ -1,27 +1,61 @@
 #include <stdio.h>
 
+int check_digit(long long int i, int n)
+{
+	char d;
+	char before_d = 'l';
+	char *hex = "0123456789abcdefghijk";
+	while (i > 0)
+	{
+		d = hex[i%21];
+		if (before_d <= d || d > hex[n])
+			return (1);
+		i = i/21;
+		before_d = d;
+	}
+	return (0);
+}
+
+long long int max_hex(int n)
+{
+	char *hex = "0123456789abcdefghijk";
+	long long int sum = 0;
+	for(int i = 1; i <= n; i ++)
+	{
+		if('0' < hex[i] && hex[i] <= '9')
+			sum = sum * 21 + hex[i] - '0';
+		else
+			sum = sum * 21 + hex[i] - 87;
+	}
+	return (sum);
+}
+
 int main(void)
 {
 	int n;
 	int s;
-	int num[20];
-	int sum;
+	int num[21];
+	long long int sum;
+	long long int j;
 	int cnt = 0;
 
 	scanf("%d %d", &n, &s);
-	for (int i = 0; i < n; i ++)
-		scanf("%d", &num[i]);
 	for (int i = 1; i <= n; i ++)
+		scanf("%d", &num[i]);
+	for(long long int i = 1; i <= max_hex(n); i ++)
 	{
-		for (int j = 0; j <= n - i; j ++)
+		if (check_digit(i, n) == 0)
 		{
 			sum = 0;
-			for (int k = 0; k < i; k ++)
-				sum += num[j + k];
-			if (sum == s)
+			for(int j = i; j > 0; j = j/21)
+			{
+
+				sum += num[j%21];
+			}
+			if(sum == s)
 				cnt ++;
 		}
 	}
-	printf("%d", cnt);
+	printf("%d\n",cnt);
 	return (0);
 }
