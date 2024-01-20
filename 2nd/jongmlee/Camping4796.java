7e5cd38cf031ce5aa76568f6d27ae78a61d57013
이종민의 맥북 <jongmlee@ijongmin-uiui-noteubug.local>
2024-01-21 01:26:30 +0900

    4796

--- 2nd/jongmlee/Camping4796.java
+++ 2nd/jongmlee/Camping4796.java
@@ -1,29 +1,29 @@
 import java.util.*;
 
 class  Camping4796{
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
 
-        int p, l, v, answer;
+		int p, l, v, answer;
 		int i = 0;
-        while (true) {
+		while (true) {
 			answer = 0;
 			l = sc.nextInt(); // l일을 사용 가능
-            p = sc.nextInt(); // 연속하는 p일 중
-            v = sc.nextInt(); // v짜리 휴가
+			p = sc.nextInt(); // 연속하는 p일 중
+			v = sc.nextInt(); // v짜리 휴가
 			if (l == 0 && p == 0 && v == 0) {
 				break;
 			}
-            while (v > 0) {
-                if (v > p) {
-                    v -= p;
-                    answer += l;
-                } else {
+			while (v > 0) {
+				if (v > p) {
+					v -= p;
+					answer += l;
+				} else {
 					answer = v > l ? answer + l : answer + v;
 					v = 0;
 				}
 			}
 			System.out.printf("Case %d: %d\n", ++i , answer);
 		}
-    }
+	}
 }
