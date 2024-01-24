487fb4624721440374d8f4944a6cd60b0369942a
jeayoungKim529 <jeay529@gmail.com>
2024-01-24 16:50:01 +0900

    24.01.25

--- 3rd/jeakim/2164.cpp
+++ 3rd/jeakim/2164.cpp
@@ -3,20 +3,20 @@
 
 using namespace std;
 
-// int main()
-// {
-// 	queue<int> que;
-// 	int N;
+int main()
+{
+	queue<int> que;
+	int N;
 
-// 	cin >> N;
-// 	for (int i = 1; i <= N; i++)
-// 		que.push(i);
-// 	while (que.size() > 1)
-// 	{
-// 		que.pop();
-// 		que.push(que.front());
-// 		que.pop();
-// 	}
-// 	cout << que.front() << endl;
-// 	return (0);
-// }
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+		que.push(i);
+	while (que.size() > 1)
+	{
+		que.pop();
+		que.push(que.front());
+		que.pop();
+	}
+	cout << que.front() << endl;
+	return (0);
+}
