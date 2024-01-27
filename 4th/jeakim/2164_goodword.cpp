1dad4dd8c1c9012aebecb80f2701eb7f6c892915
jeayoungKim529 <jeay529@gmail.com>
2024-01-27 17:13:32 +0900

    24.01.29

---
+++ 4th/jeakim/2164_goodword.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	stack<char> st;
+	string s;
+	int N;
+	int cnt;
+
+	cnt = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> s;
+		for (int j = 0; j < s.size(); j++)
+		{
+			if (st.empty() || st.top() != s[j])
+				st.push(s[j]);
+			else if (st.top() == s[j])
+				st.pop();
+		}
+		if (st.empty())
+			cnt++;
+		cout << "cnt : " << cnt << endl;
+		while(st.empty()) st.pop();
+	}
+	cout << cnt << endl;
+	return (0);
+}
