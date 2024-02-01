24e858f920d5066589e372b46f987e1ae4f18dbb
jeayoungKim529 <jeay529@gmail.com>
2024-02-01 18:17:09 +0900

    knight

--- 5th/jeakim/7562_knight.cpp
+++ 5th/jeakim/7562_knight.cpp
@@ -4,19 +4,32 @@
 
 using namespace std;
 
-int count_move(int board[300][300], int visit[300][300], pair<int, int> cur, pair<int, int> target)
+int count_move(int board[300][300], int visit[300][300], int l, pair<int, int> start, pair<int, int> target)
 {
 	int dx[8] = {-1, -2, -2, -1, 1, 2, 2, 1};
 	int dy[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
 	int cnt = 0;
 
 	queue<pair<int, int> > que;
-	que.push(cur);
-	while()
-	for(int i = 0; i < 8; i++){
-
+	visit[start.first][start.second] = 0;
+	que.push(start);
+	while(!que.empty()){
+		pair<int, int> cur = que.front();
+		que.pop();
+		if (cur == target)
+			break;
+		for(int i = 0; i < 8; i++){
+			int cur_x = cur.first + dx[i];
+			int cur_y = cur.second + dy[i];
+			if (cur_x >= l || cur_x < 0 || cur_y >= l || cur_y < 0)
+				continue;
+			if (visit[cur_x][cur_y] >= 0)
+				continue;
+			visit[cur_x][cur_y] = visit[cur.first][cur.second] + 1;
+			que.push(make_pair(cur_x, cur_y));
+		}
 	}
-	return (0);
+	return (visit[target.first][target.second]);
 }
 
 int main()
@@ -29,17 +42,17 @@ int main()
 	cin >> T;
 	for (int t = 0; t < T; t++){
 		int board[300][300] = {0};
-		int visit[300][300] = {0};
+		int visit[300][300] = {-1};
 		//
 		int l;
 		cin >> l;
 		int x, y;
 		cin >> x >> y;
-		pair<int, int> cur = make_pair(x, y);
+		pair<int, int> start = make_pair(x, y);
 		cin >> x >> y;
 		pair<int, int> target = make_pair(x, y);
 		int cnt;
-		cnt = count_move(board, visit, cur, target);
+		cnt = count_move(board, visit, l, start, target);
 		cout << cnt << endl;
 	}
 	return (0);
