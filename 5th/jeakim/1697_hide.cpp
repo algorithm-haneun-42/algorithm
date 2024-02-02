671d45eddcae3f6b008102667b363105388e86db
jeayoungKim529 <jeay529@gmail.com>
2024-02-02 20:50:02 +0900

    hide

--- 5th/jeakim/1697_hide.cpp
+++ 5th/jeakim/1697_hide.cpp
@@ -13,35 +13,33 @@ int main()
 {
 	int N, K;
 	queue<int> que;
-	int visit[100000];
+	int visit[100000] = {0};
 
 	cin >> N >> K;
-	for (int i = 0; i < 100000; i++)
-		visit[i] = -1;
 	que.push(N);
-	visit[N] = 0;
+	visit[N] = 1;
 	while(!que.empty()){
 		int cur = que.front();
+		que.pop();
 		if (cur == K)
 			break;
-		que.pop();
 		int plus = cur + 1;
-		if (visit[plus] == -1 && 0 <= plus && plus < 100000){
+		if (0 <= plus && plus < 100000 && visit[plus] == 0){
 			que.push(plus);
 			visit[plus] = visit[cur] + 1;
 		}
 		int minus = cur - 1;
-		if (visit[minus] == -1 && 0 <= minus && minus < 100000){
+		if (0 <= minus && minus < 100000 && visit[minus] == 0){
 			que.push(minus);
 			visit[minus] = visit[cur] + 1;
 		}
 		int multi = cur * 2;
-		if (visit[multi] == -1 && 0 <= multi && multi < 100000){
+		if (0 <= multi && multi < 100000 && visit[multi] == 0){
 			que.push(multi);
 			visit[multi] = visit[cur] + 1;
 		}
 	}
 	//print_visit(visit, N, K);
-	cout << endl << visit[que.front()] << endl;
+	cout << visit[K] - 1 << endl;
 	return (0);
 }
