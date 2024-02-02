0bb50a9f9a37499257be01ee0fbe14838c41fc17
jeayoungKim529 <jeay529@gmail.com>
2024-02-02 14:55:42 +0900

    tomato

--- 5th/jeakim/7569_tomato.cpp
+++ 5th/jeakim/7569_tomato.cpp
@@ -20,64 +20,41 @@ int check_flag(int tomato[100][100][100], int M, int N, int H)
 	return (flag);
 }
 
-int count_days(int tomato[100][100][100], int visit[100][100][100], int M, int N, int H)
+int count_days(int tomato[100][100][100], int visit[100][100][100], queue<pair<pair<int, int>, int> > que, int M, int N, int H)
 {
 	int max_day = 1;
-	queue<pair<pair<int, int>, int> > que;
+	
 	int dm[6] = {1, 0, 0, -1, 0, 0};
 	int dn[6] = {0, 1, 0, 0, -1, 0};
 	int dh[6] = {0, 0, 1, 0, 0, -1};
 
-	for (int m = 0; m < M; m++){
-		for (int n = 0; n < N; n++){
-			for (int h = 0; h < H; h++){
-				if (tomato[m][n][h] == 0 || tomato[m][n][h] == -1)
-					continue;
-				if (visit[m][n][h] > 0)
-					continue;
-				que.push(make_pair(make_pair(m, n), h));
-				visit[m][n][h] = 1;
-				while (!que.empty()){
-					pair<pair<int, int>, int> cur = que.front();
-					que.pop();
-					tomato[cur.first.first][cur.first.second][cur.second] = 1;
-					for (int d = 0; d < 6; d++){
-						int cur_m = cur.first.first + dm[d];
-						int cur_n = cur.first.second + dn[d];
-						int cur_h = cur.second + dh[d];
-						if (cur_m < 0 || cur_n < 0 || cur_h < 0 || cur_m >= M || cur_n >= N || cur_h >= H)
-							continue;
-						if (tomato[cur_m][cur_n][cur_h] == -1 || tomato[cur_m][cur_n][cur_h] == 1)
-							continue;
-						if (visit[cur_m][cur_n][cur_h] > 0)
-							continue;
-						visit[cur_m][cur_n][cur_h] = visit[cur.first.first][cur.first.second][cur.second] + 1;
-						que.push(make_pair(make_pair(cur_m, cur_n), cur_h));
-						max_day = max(max_day, visit[cur_m][cur_n][cur_h]);
-					}
-				}
-			}
+	pair<pair<int, int>, int> cur = que.front();
+	visit[cur.first.first][cur.first.second][cur.second] = 1;
+	while (!que.empty()){
+	pair<pair<int, int>, int> cur = que.front();
+	que.pop();
+	tomato[cur.first.first][cur.first.second][cur.second] = 1;
+		for (int d = 0; d < 6; d++){
+			int cur_m = cur.first.first + dm[d];
+			int cur_n = cur.first.second + dn[d];
+			int cur_h = cur.second + dh[d];
+			if (cur_m < 0 || cur_n < 0 || cur_h < 0 || cur_m >= M || cur_n >= N || cur_h >= H)
+				continue;
+			if (tomato[cur_m][cur_n][cur_h] == -1 || tomato[cur_m][cur_n][cur_h] == 1)
+				continue;
+			if (visit[cur_m][cur_n][cur_h] > 0)
+				continue;
+			visit[cur_m][cur_n][cur_h] = visit[cur.first.first][cur.first.second][cur.second] + 1;
+			que.push(make_pair(make_pair(cur_m, cur_n), cur_h));
+			max_day = max(max_day, visit[cur_m][cur_n][cur_h]);
 		}
 	}
 	return (max_day -1);
 }
 
-// int count_max(int visit[100][100][100], int M, int N, int H)
-// {
-// 	int max_day = 0;
-
-// 	for (int m = 0; m < M; m++){
-// 		for (int n = 0; n < N; n++){
-// 			for (int h = 0; h < H; h++){
-// 				max_day = max(max_day, visit[m][n][h]);
-// 			}
-// 		}
-// 	}
-// 	return (max_day - 1);
-// }
-
 void print_visit(int visit[100][100][100], int M, int N, int H)
 {
+	cout << endl;
 	for (int m = 0; m < M; m++){
 		for (int n = 0; n < N; n++){
 			for (int h = 0; h < H; h++){
@@ -97,6 +74,7 @@ int main()
 	int M, N, H;
 	int tomato[100][100][100];
 	int visit[100][100][100] = {0};
+	queue<pair<pair<int, int>, int> > start;
 	int max_day;
 
 	cin >> H >> N >> M;
@@ -104,14 +82,19 @@ int main()
 		for (int n = 0; n < N; n++){
 			for (int h = 0; h < H; h++){
 				cin >> tomato[m][n][h];
+				if (tomato[m][n][h] == 1){
+					start.push(make_pair(make_pair(m, n), h));
+					visit[m][n][h] = 1;
+				}
 			}
 		}
 	}
-	max_day = count_days(tomato, visit, M, N, H);
+	max_day = count_days(tomato, visit, start, M, N, H);
 	//print_visit(visit, M, N, H);
+	//print_visit(tomato, M, N, H);
 	if (check_flag(tomato, M, N, H) == -1)
 		cout << -1 << endl;
 	else
-		cout << max_day << endl ;
+		cout << max_day << endl;
 	return (0);
 }
