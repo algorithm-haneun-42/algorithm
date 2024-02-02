0e4594a3bc5372a7c1f292bd5ea226068b237a3d
jeayoungKim529 <jeay529@gmail.com>
2024-02-02 10:45:06 +0900

    knight

--- 5th/jeakim/7562_knight.cpp
+++ 5th/jeakim/7562_knight.cpp
@@ -11,49 +11,50 @@ int count_move(int board[300][300], int visit[300][300], int l, pair<int, int> s
 	int cnt = 0;
 
 	queue<pair<int, int> > que;
-	visit[start.first][start.second] = 0;
+	visit[start.first][start.second] = 1;
 	que.push(start);
 	while(!que.empty()){
 		pair<int, int> cur = que.front();
 		que.pop();
-		if (cur == target)
+		if (cur.first == target.first && cur.second == target.second)
 			break;
 		for(int i = 0; i < 8; i++){
 			int cur_x = cur.first + dx[i];
 			int cur_y = cur.second + dy[i];
 			if (cur_x >= l || cur_x < 0 || cur_y >= l || cur_y < 0)
 				continue;
-			if (visit[cur_x][cur_y] >= 0)
+			if (visit[cur_x][cur_y] > 0)
 				continue;
 			visit[cur_x][cur_y] = visit[cur.first][cur.second] + 1;
 			que.push(make_pair(cur_x, cur_y));
 		}
 	}
-	return (visit[target.first][target.second]);
+	return (visit[target.first][target.second] - 1);
 }
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+// int main()
+// {
+// 	ios::sync_with_stdio(0);
+// 	cin.tie(0);
 
-	int T;
+// 	int T;
 
-	cin >> T;
-	for (int t = 0; t < T; t++){
-		int board[300][300] = {0};
-		int visit[300][300] = {-1};
-		//
-		int l;
-		cin >> l;
-		int x, y;
-		cin >> x >> y;
-		pair<int, int> start = make_pair(x, y);
-		cin >> x >> y;
-		pair<int, int> target = make_pair(x, y);
-		int cnt;
-		cnt = count_move(board, visit, l, start, target);
-		cout << cnt << endl;
-	}
-	return (0);
-}
+// 	cin >> T;
+// 	for (int t = 0; t < T; t++){
+// 		int board[300][300] = {0};
+// 		int visit[300][300] = {0};
+// 		//
+// 		int l;
+// 		cin >> l;
+// 		int x, y;
+// 		cin >> x >> y;
+// 		pair<int, int> start = make_pair(x, y);
+// 		cin >> x >> y;
+// 		pair<int, int> target = make_pair(x, y);
+// 		//
+// 		int cnt;
+// 		cnt = count_move(board, visit, l, start, target);
+// 		cout << cnt << endl;
+// 	}
+// 	return (0);
+// }
