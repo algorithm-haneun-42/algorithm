414a750dcbc9df042490d9ea3782229ccd82c38d
jeayoungKim529 <jeay529@gmail.com>
2024-02-03 13:40:36 +0900

    hide

--- 5th/jeakim/1697_hide.cpp
+++ 5th/jeakim/1697_hide.cpp
@@ -9,37 +9,40 @@ using namespace std;
 // 		cout << visit[i] << ' ';
 // }
 
-int main()
-{
-	int N, K;
-	queue<int> que;
-	int visit[100000] = {0};
+// int main()
+// {
+// 	ios::sync_with_stdio(0);
+// 	cin.tie(0);
 
-	cin >> N >> K;
-	que.push(N);
-	visit[N] = 1;
-	while(!que.empty()){
-		int cur = que.front();
-		que.pop();
-		if (cur == K)
-			break;
-		int plus = cur + 1;
-		if (0 <= plus && plus < 100000 && visit[plus] == 0){
-			que.push(plus);
-			visit[plus] = visit[cur] + 1;
-		}
-		int minus = cur - 1;
-		if (0 <= minus && minus < 100000 && visit[minus] == 0){
-			que.push(minus);
-			visit[minus] = visit[cur] + 1;
-		}
-		int multi = cur * 2;
-		if (0 <= multi && multi < 100000 && visit[multi] == 0){
-			que.push(multi);
-			visit[multi] = visit[cur] + 1;
-		}
-	}
-	//print_visit(visit, N, K);
-	cout << visit[K] - 1 << endl;
-	return (0);
-}
+// 	int N, K;
+// 	queue<int> que;
+// 	int visit[100000] = {0};
+
+// 	cin >> N >> K;
+// 	que.push(N);
+// 	visit[N] = 1;
+// 	while(visit[K] == 0){
+// 		int cur = que.front();
+// 		que.pop();
+// 		// if (cur == K)
+// 		// 	break;
+// 		int plus = cur + 1;
+// 		if (0 <= plus && plus < 100000 && visit[plus] == 0){
+// 			que.push(plus);
+// 			visit[plus] = visit[cur] + 1;
+// 		}
+// 		int minus = cur - 1;
+// 		if (0 <= minus && minus < 100000 && visit[minus] == 0){
+// 			que.push(minus);
+// 			visit[minus] = visit[cur] + 1;
+// 		}
+// 		int multi = cur * 2;
+// 		if (0 <= multi && multi < 100000 && visit[multi] == 0){
+// 			que.push(multi);
+// 			visit[multi] = visit[cur] + 1;
+// 		}
+// 	}
+// 	//print_visit(visit, N, K);
+// 	cout << visit[K] - 1 << endl;
+// 	return (0);
+// }
