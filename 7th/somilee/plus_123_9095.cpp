b0dd930ff8c438a9ffbbe9216b52078eec5e7677
Somin Lee <somilee@c2r2s3.42seoul.kr>
2024-02-07 19:31:37 +0900

    9095

--- 7th/somilee/plus_123_9095.cpp
+++ 7th/somilee/plus_123_9095.cpp
@@ -2,29 +2,22 @@
 
 using namespace std;
 
-int plus_123(int num)
-{
-	int cnt = 0;
-	if (num >0)
-	{
-		cnt += plus_123(num - 1);
-		cnt += plus_123(num - 2);
-		cnt += plus_123(num - 3);
-	}
-	if (num == 0)
-		cnt ++;
-	return (cnt);
-}
-
 int main(void)
 {
-	int n;
-	cin >> n;
-	
-	for (int i = 0; i < n; i ++)
+	int num;
+	cin >> num;
+
+	for (int i = 0; i < num; i ++)
 	{
-		int num;
-		cin >> num;
-		cout << plus_123(num) << '\n';
+		int n;
+		cin >> n;
+		
+		int d[100000];
+		d[1] = 1;
+		d[2] = 2;
+		d[3] = 4;
+		for (int i = 4; i <= n; i ++)
+			d[i] = d[i-1] + d[i-2] + d[i-3];
+		cout << d[n] << '\n';
 	}
 }
