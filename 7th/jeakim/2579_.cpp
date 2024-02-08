604ae484fd346ad0df6de3f33b0138cde2818520
jeayoungKim529 <jeay529@gmail.com>
2024-02-08 21:14:15 +0900

    [FAIL]2579

--- 7th/jeakim/2579_.cpp
+++ 7th/jeakim/2579_.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cmath>
 
-
 using namespace std;
+
 int main()
 {
 	int stair[301];
@@ -14,7 +14,7 @@ int main()
 	}
 	long score = stair[1];
 	int idx = 1;
-	while (idx < N - 1){
+	while (idx < N - 1){ //한칸 이동하는게 연달아 있는 경우 생각 필요!
 		score += max(stair[idx + 1], stair[idx + 2]);
 		idx += stair[idx + 1] > stair[idx + 2] ? 1 : 2;
 	}
