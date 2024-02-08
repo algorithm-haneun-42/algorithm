385e4f068d022de83ad329f84356bd36fb6a7cdc
jeayoungKim529 <jeay529@gmail.com>
2024-02-08 21:11:18 +0900

    [FAIL]2011

---
+++ 7th/jeakim/2011_password_cord.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s;
+	int cnt = 0;
+	cin >> s;
+	for (int i = 1; i < s.size(); i++){
+		if ()
+	}
+
+	return (0);
+}
