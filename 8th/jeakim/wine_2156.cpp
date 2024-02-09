365b7d82d6cd158afabd45a62eff830a1cd6d265
jeayoungKim529 <jeay529@gmail.com>
2024-02-09 21:27:51 +0900

    [ING]2156

---
+++ 8th/jeakim/wine_2156.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int cup[10000];
+	int score[10000];
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> cup[i];
+	
+	return (0);
+}
