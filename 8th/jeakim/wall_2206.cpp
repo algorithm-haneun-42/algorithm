d9985b6dd8d691ca6c016f1cbe3849fe39b835d2
jeayoungKim529 <jeay529@gmail.com>
2024-02-09 21:22:25 +0900

    wall

---
+++ 8th/jeakim/wall_2206.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, M;
+	int map[1000][1000];
+	int path[1000][1000];
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			cin >> map[i][j];
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < M; j++){
+			
+		}
+	}
+	return (0);
+}
