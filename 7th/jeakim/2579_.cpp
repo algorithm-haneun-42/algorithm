f43eeafc6e7b8d626247cd118a170739959ab934
jeayoungKim529 <jeay529@gmail.com>
2024-02-14 14:14:12 +0900

    2579

--- 7th/jeakim/2579_.cpp
+++ 7th/jeakim/2579_.cpp
@@ -6,18 +6,22 @@ using namespace std;
 int main()
 {
 	int stair[301];
+	int score[301][3];
 	int N;
 
 	cin >> N;
-	for (int i = 1; i <= N; i++){
+	for (int i = 1; i <= N; i++)
 		cin >> stair[i];
+	score[1][1] = stair[1];
+	score[1][2] = 0;
+	score[2][1] = stair[2];
+	score[2][2] = stair[1] + stair[2];
+	for (int i = 3; i <= N; i++)
+	{
+		score[i][1] = max(score[i - 2][1], score[i - 2][2]) + stair[i];
+		score[i][2] = score[i - 1][1] + stair[i];
 	}
-	long score = stair[1];
-	int idx = 1;
-	while (idx < N - 1){ //한칸 이동하는게 연달아 있는 경우 생각 필요!
-		score += max(stair[idx + 1], stair[idx + 2]);
-		idx += stair[idx + 1] > stair[idx + 2] ? 1 : 2;
-	}
-	cout << score << endl;
+
+	cout << max(score[N][1], score[N][2]) << endl;
 	return (0);
 }
