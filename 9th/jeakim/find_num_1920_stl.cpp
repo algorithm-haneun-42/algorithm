ddd450bf3ff9b6fcf5d60967fa37235428304187
jeayoungKim529 <jeay529@gmail.com>
2024-02-18 17:47:23 +0900

    [SCS]1920 : endl -> \n

--- 9th/jeakim/find_num_1920_stl.cpp
+++ 9th/jeakim/find_num_1920_stl.cpp
@@ -4,29 +4,11 @@
 
 using namespace std;
 
-int binarysearch(int n, int a[100000], int trg)
-{
-	int st = 0;
-	int en = n - 1;
-
-	while (st <= en)
-	{
-		int mid = (st + en) / 2;
-
-		if (a[mid] < trg)
-			st = mid + 1;
-		else if (a[mid] > trg)
-			en = mid - 1;
-		else
-			return (1);
-	}
-	return (0);
-}
-
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	cout.tie(0);
 
 	int n, m, k;
 	vector<int> a;
@@ -42,6 +24,6 @@ int main()
 	for (int i = 0; i < m; i++)
 	{
 		cin >> k;
-		cout << binary_search(a.begin(), a.end(), k) << endl;
+		cout << binary_search(a.begin(), a.end(), k) << '\n';
 	}
 }
