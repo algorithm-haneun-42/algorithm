f16dd6bb0d8dc6e35fd7b3298b06db2ca8691818
jeayoungKim529 <jeay529@gmail.com>
2024-03-11 15:12:01 +0900

    [SCS]14501

--- 15th/jeakim/resign_14501.cpp
+++ 15th/jeakim/resign_14501.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int count_benefit(int n, int schedule[16][2], int benefit[16])
+void count_benefit(int n, int schedule[16][2], int dp[17])
 {
-	int dp[16];
-	int max_point;
-
-	for (int i = 0; i < n; i++){
-		
+	for (int i = n; i >= 0; i--){
+		int pos = i + schedule[i][0];
+		if (pos > n + 1)
+			dp[i] = dp[i + 1]; //i일에 상담x
+		else
+			dp[i] = max(dp[i + 1], schedule[i][1] + dp[i + schedule[i][0]]);
 	}
 }
 
@@ -16,12 +18,15 @@ int main()
 {
 	int n;
 	int schedule[16][2];
-	int benefit[16] = {0};
+	int dp[17] = {0};
+	int max_p = 0;
 
 	cin >> n;
-	for (int i = 1; i < n; i++)
+	for (int i = 1; i <= n; i++)
 		cin >> schedule[i][0] >> schedule[i][1];
-	
-	cout << count_benefit(n, schedule, benefit) << '\n';
+	count_benefit(n, schedule, dp);
+	for (int i = 1; i <= n; i++)
+		max_p = max(max_p, dp[i]);
+	cout << max_p << '\n';
 	return (0);
 }
