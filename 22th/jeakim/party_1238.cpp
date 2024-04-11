b392c24e242aa417191ee007b144a936fe967c0e
jeayoungKim529 <jeay529@gmail.com>
2024-04-11 17:06:11 +0900

    [SCS]1238

--- 22th/jeakim/party_1238.cpp
+++ 22th/jeakim/party_1238.cpp
@@ -25,24 +25,44 @@ int main()
 
 	int max_n = 0;
 	for (int st = 1; st <= n; st++){
-		priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int>> > pq;
+		int go, back;
+		priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int>> > pq_go;
 		for (int i = 1; i <= n; i++)
 			d[i] = INF;
 		d[st] = 0;
-		pq.push({d[st], st});
-		while (!pq.empty()){
-			auto cur = pq.top();
-			pq.pop();
+		pq_go.push({d[st], st});
+		while (!pq_go.empty()){
+			auto cur = pq_go.top();
+			pq_go.pop();
 			if (d[cur.second] != cur.first)
 				continue;
 			for (auto nxt : adj[cur.second]){
 				if (d[nxt.second] <= d[cur.second] + nxt.first)
 					continue;
 				d[nxt.second] = d[cur.second] + nxt.first;
-				pq.push({d[nxt.second], nxt.second});
+				pq_go.push({d[nxt.second], nxt.second});
 			}
 		}
-		max_n = max(max_n, d[x]);
+		go = d[x];
+		priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int>> > pq_b;
+		for (int i = 1; i <= n; i++)
+			d[i] = INF;
+		d[x] = 0;
+		pq_b.push({d[x], x});
+		while (!pq_b.empty()){
+			auto cur = pq_b.top();
+			pq_b.pop();
+			if (d[cur.second] != cur.first)
+				continue;
+			for (auto nxt : adj[cur.second]){
+				if (d[nxt.second] <= d[cur.second] + nxt.first)
+					continue;
+				d[nxt.second] = d[cur.second] + nxt.first;
+				pq_b.push({d[nxt.second], nxt.second});
+			}
+		}
+		back = d[st];
+		max_n = max(max_n, go + back);
 	}
 	cout << max_n << '\n';
 	return (0);
