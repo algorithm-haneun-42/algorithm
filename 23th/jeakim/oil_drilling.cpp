687c5abc3c5edac430ff9120ed8b81b0ee16ef69
jeayoungKim529 <jeay529@gmail.com>
2024-04-23 17:24:35 +0900

    [ING]

--- 23th/jeakim/oil_drilling.cpp
+++ 23th/jeakim/oil_drilling.cpp
@@ -3,43 +3,63 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <map>
+#include <set>
 
 using namespace std;
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 int n, m;
+map<int, int> oil_size;
+int visit[501][501] = {0};
 
-int count_oil(vector<vector<int>> land, int col)
+void find_oil(vector<vector<int> > land)
 {
-    int oil = 0;
-    int visit[501][501] = {0};
     queue<pair<int, int> > q;
-
-    for (int row = 0; row < n; row++){
-        if (land[row][col] == 1 && visit[row][col] == 0){
-            q.push({row, col});
-            visit[row][col] = 1;
-            while (!q.empty()){
-                pair<int, int> cur;
-                cur = q.front();
-                q.pop();
-                visit[cur.first][cur.second] = 1;
-                oil++;
-                for (int i = 0; i < 4; i++){
-                    int x = cur.first + dx[i];
-                    int y = cur.second + dy[i];
-                    if (x < 0 || y < 0 || x > n - 1 || y > m - 1)
-                        continue;
-                    if (land[x][y] == 1 && visit[x][y] == 0){
-                        visit[x][y] = 1;
-                        q.push({x, y});
+    int cnt = 1;
+    
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            if (land[i][j] == 1 && visit[i][j] == 0){
+                q.push({i, j});
+                visit[i][j] = cnt;
+                int oil = 0;
+                while (!q.empty()){
+                    pair<int, int> cur;
+                    cur = q.front();
+                    q.pop();
+                    visit[cur.first][cur.second] = cnt;
+                    oil++;
+                    for (int k = 0; i < 4; k++){
+                        int x = cur.first + dx[i];
+                        int y = cur.second + dy[i];
+                        if (x < 0 || y < 0 || x >= n || y >= m)
+                            continue;
+                        if (land[x][y] == 1 && visit[x][y] == 0){
+                            visit[x][y] = 1;
+                            q.push({x, y});
+                        }
                     }
                 }
+                oil_size.insert({cnt, oil});
+                cnt++;
             }
         }
     }
-    return (oil);
+}
+
+int count_oil(vector<vector<int>> land, int col)
+{
+    set<int> num;
+    int oil = 0;
+    
+    for (int i = 0; i < n; i++){
+        num.insert(visit[i][col]);
+    }
+    for (auto it = num.begin(); it != num.end(); it++){
+        oil += oil_size.find(*it);
+    }
 }
 
 int solution(vector<vector<int>> land) {
@@ -47,7 +67,10 @@ int solution(vector<vector<int>> land) {
     
     n = land.size();
     m = land[0].size();
-    for (int j = 0; j < m; j++)
-        answer = max(answer, count_oil(land, j));
+    oil_size.insert({0, 0});
+    find_oil(land);
+    for (int i = 0; i < oil_size.size(); i++)
+        answer = max(answer, count_oil(land));
+    
     return answer;
 }
