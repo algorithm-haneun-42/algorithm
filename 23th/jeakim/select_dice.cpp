ddd0aa792901620b86fd12fdc317e6e692d90d11
jeayoungKim529 <jeay529@gmail.com>
2024-04-23 21:12:19 +0900

    [ING]

--- 23th/jeakim/select_dice.cpp
+++ 23th/jeakim/select_dice.cpp
@@ -1,34 +1,59 @@
 #include <string>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
+struct s_time{
+	double h;
+	double m;
+	double s;
+}t_time;
+
+s_time getTime(int h, int m, int s){
+	s_time t;
+
+	t.h = 5 * h + m / 12 + s / 60;
+	t.m = m + s / 60;
+	t.s = s;
+	return (t);
+}
+
 int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
-    int answer = -1;
+    int answer = 0;
+	s_time str = getTime(h1, m1, s1);
+	s_time end = getTime(h2, m2, s2);
+	s_time cur = {(double)h1, (double)m1, (double)s1};
 
-	int h, m, s, cnt;
-
-	h = 5 * h + m1 / 12 + s / 60;
-	m = m1 + s / 60;
-	s = s1;
-
-	while (h != h2 && m != m2 && s != s2){
-		if (h == s && m == s)
+	while (str.h != end.h || str.m != end.m || str.s != end.s){
+		if (str.h == str.s && str.m == str.s)
 			answer++;
-		else if (h == s)
+		else if (str.h == str.s)
 			answer++;
-		else if (m == s)
-			cnt++;
-		if (s == 60){
-			s == 0;
-			m++;
+		else if (str.m == str.s)
+			answer++;
+		cur.s++;
+		if (cur.s == 60){
+			cur.s = 0;
+			cur.m++;
 		}
-		if (m == 60){
-			m == 0;
-			h++;
+		if (cur.m == 60){
+			cur.m = 0;
+			cur.h++;
 		}
-		s++;
+		str = getTime(cur.h, cur.m, cur.s);
 	}
-
     return answer;
+}
+
+int main()
+{
+	int h1, m1, s1, h2, m2, s2;
+
+	h1 = 1; m1 = 5; s1 = 5;
+	h2 = 1; m2 = 5; s2 = 6;
+
+	cout << solution(h1, m1, s1, h2, m2, s2) << endl;
+
+	return (0);
 }
