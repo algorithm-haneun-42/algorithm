e4a49c0a69792c83c33248bad75d88dcbbcfb7ee
jeayoungKim529 <jeay529@gmail.com>
2024-04-27 14:25:01 +0900

    [FAIL] 1, 3

---
+++ 25th/jeakim/magical_elebator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int storey) {
+    int answer = 0;
+
+	while (storey > 0){
+		if (storey % 10 == 5 && storey / 10 % 10 > 5){
+			answer += 5;
+			storey += 10;
+		}
+		else if (storey % 10 > 5){
+			answer += 10 - storey % 10;
+			storey += 10;
+		}
+		else
+			answer += storey % 10;
+		storey /= 10;
+	}
+    return answer;
+}
+
+int main()
+{
+	cout << solution(555) << endl;
+	return (0);
+}
