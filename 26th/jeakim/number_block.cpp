a533d522afe2c820a5704ab1b9040557e28cf99f
jeayoungKim529 <jeay529@gmail.com>
2024-05-01 17:21:09 +0900

    [FAIL] 66.7 : 13 & 1-6

--- 26th/jeakim/number_block.cpp
+++ 26th/jeakim/number_block.cpp
@@ -13,19 +13,16 @@ long long max_com_div(long long n)
     if (n % 2 == 0)
         return (n / 2);
 	m = 3;
-    for (int i = 3; i * i <= n; i = i + 2){
-        if (n % i == 0){
-			
+    for (int i = 3; i <= 10000000 && i * i <= n; i = i + 2)
+        if (n % i == 0)
             return (n / i);
-		}
-	}
     return (1);
 }
 
 vector<int> solution(long long begin, long long end) {
     vector<int> answer;
     
-    for (int i = begin; i <= end; i++)
+    for (long long i = begin; i <= end; i++)
         answer.push_back(max_com_div(i));
     return answer;
 }
@@ -33,7 +30,7 @@ vector<int> solution(long long begin, long long end) {
 int main()
 {
 	vector<int> answer = solution(1, 10);
-	for (int i = 0; i < answer.size(); i++){
+	for (long long i = 0; i < answer.size(); i++){
 		cout << answer[i] << ' ';
 	}
 	return (0);
