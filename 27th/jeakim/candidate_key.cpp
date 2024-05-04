47257d17decdbc174b1bb0bb8763f9a122d7f847
jeayoungKim529 <jeay529@gmail.com>
2024-05-04 17:36:20 +0900

    [ING]

--- 27th/jeakim/candidate_key.cpp
+++ 27th/jeakim/candidate_key.cpp
@@ -44,15 +44,17 @@ int solution(vector<vector<string>> relation) {
 	for (int i = 0; i < col; i++)
 		dfs(1, i);
 	answer = cnt;
+	if (answer == 0)
+		return (1);
     return answer;
 }
 
 int main()
 {
-	vector<vector<string> > relation = {{"100","ryan","music","2"},\
-		{"200","apeach","math","2"},{"300","tube","computer","3"},\
-		{"400","con","computer","4"},{"500","muzi","music","3"},\
-		{"600","apeach","music","2"}};
+	vector<vector<string> > relation = { {"a","1","aaa","c","ng"},
+			{"a","1","bbb","e","g"},
+			{"c","1","aaa","d","ng"},
+			{"d","2","bbb","d","ng"}};
 
 	cout << solution(relation) << endl;
 
