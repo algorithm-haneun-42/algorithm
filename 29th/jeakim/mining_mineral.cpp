4b9711dd1b07fd72d24a38903872486e8711fd08
jeayoungKim529 <jeay529@gmail.com>
2024-05-13 16:55:45 +0900

    [ING]

--- 29th/jeakim/mining_mineral.cpp
+++ 29th/jeakim/mining_mineral.cpp
@@ -7,19 +7,22 @@
 using namespace std;
 int pickax = 0;
 int point[10][3] = {0};
+int n = 0;
 
 int find_small(vector<int> picks, vector<string> minerals)
 {
 	int answer = 0;
+	int p = 0;
 
 	for (int k = 0; k < 3; k++){
 		priority_queue<int, vector<int>, greater<int>> pq;
-		for (int i = 0; i < pickax + 1 && i < minerals.size() / 5 + 1; i++)
+		for (int i = 0; i < pickax && i < n; i++)
 			pq.push(point[i][k]);
-		for (int i = 0; i < picks[0]; i++){
+		for (int i = 0; i < picks[k] && p < n; i++){
 			answer += pq.top();
 			pq.pop();
-			cout << answer << endl;
+			cout << "answer : " << answer << endl;
+			p++;
 		}
 	}
 	return (answer);
@@ -27,30 +30,30 @@ int find_small(vector<int> picks, vector<string> minerals)
 
 int solution(vector<int> picks, vector<string> minerals) {
     int answer = 0;
+	n = minerals.size() / 5;
+	if (minerals.size() % 5 > 0)
+		n++;
 	//dia, iron, stone
 	for (int i = 0; i < picks.size(); i++)
-		pickax++;
-	for (int i = 0; i < pickax + 1 && i < minerals.size();){
-		for (int j = i; j < i + 5; j++){
-			if ("dia" == minerals[j]){
-				point[i][0]++;
-				point[i][1] += 5;
-				point[i][2] += 25;
-			}
-			else if ("iron" == minerals[j]){
-				point[i][0]++;
-				point[i][1]++;
-				point[i][2] += 5;
-			}
-			else if ("stone" == minerals[j]){
-				point[i][0]++;
-				point[i][1]++;
-				point[i][2]++;
-			}
+		pickax += picks[i];
+	for (int i = 0; i < pickax * 5 + 1 && i < minerals.size(); i++){
+		if ("diamond" == minerals[i]){
+			point[i / 5][0]++;
+			point[i / 5][1] += 5;
+			point[i / 5][2] += 25;
 		}
-		i += 5;
+		else if ("iron" == minerals[i]){
+			point[i / 5][0]++;
+			point[i / 5][1]++;
+			point[i / 5][2] += 5;
+		}
+		else if ("stone" == minerals[i]){
+			point[i / 5][0]++;
+			point[i / 5][1]++;
+			point[i / 5][2]++;
+		}
 	}
-	for (int i = 0; i < pickax + 1 && i < minerals.size() / 5 + 1; i++)
+	for (int i = 0; i < pickax && i < minerals.size() / 5 + 1; i++)
 		cout << point[i][0] << " " << point[i][1] << " " << point[i][2] << endl;
 	answer = find_small(picks, minerals);
     return answer;
