d376b9b4e95176f408c2df61c8700fbfc4ef27fc
somilee <somilee@student.42seoul.kr>
2024-07-07 20:40:27 +0900

    ss

--- 45th/somilee/rest_area.cpp
+++ 45th/somilee/rest_area.cpp
@@ -5,31 +5,47 @@
 
 using namespace std;
 
-int n, m, l, a;
-vector<int> v, r;
+int n, m, l;
+int a[1005];
+
+int sum_div(int div) {
+	int sum = 0;
+	for(int i = 0; i < n; i++) {
+		sum += a[i] / div;
+		if (a[i] % div == 0)
+			sum--;
+	}
+	return sum;
+}
+
 int main(void) {
 	cin >> n >> m >> l;
-	for(int i = 0; i < n; i++) {
-		cin >> a;
-		v.push_back(a);
-	}
-	sort(v.begin(), v.end());
-	int start = 0;
-	for(int x : v) {
-		r.push_back(x - start);
-		start = x;
-	}
-	r.push_back(l - v.back());
-	int div;
-	for(div = 1; div <= v.back(); div++) {
-		int sum = 0;
-		for(int x : r) {
-			sum += x / div;
-			if (x % div == 0)
-			 	sum--;
-		}
-		if (sum == m)
-			break;
-	}
-	cout << div << endl;
+	int i;
+	for(i = 1; i <= n; i++)
+		cin >> a[i];
+	a[i] = l; 
+	for(int i = 0; i <= n; i++)
+		cout << a[i] << ' ';
+	cout << endl;
+	sort(a, a+n+1);
+	for(int i = 0; i <= n; i++)
+		a[i] = a[i+1] = a[i];
+	for(int i = 0; i <= n; i++)
+		cout << a[i] << ' ';
+	cout << endl;
+    int st = 1;
+    int en = l;
+    int result = en;
+
+    while (st <= en) {
+        int mid = (st + en) / 2;
+        if (sum_div(mid) <= m) {
+            result = mid;
+            en = mid - 1;
+        }
+		else
+            st = mid + 1;
+    }
+    cout << result << endl;
+    return 0;
 }
