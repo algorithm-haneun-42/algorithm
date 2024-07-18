f67677545c50ca7f10917cee363a6cf521d18fcc
somilee <somilee@student.42seoul.kr>
2024-07-18 18:06:44 +0900

    ss

--- 48th/somilee/LCS3.cpp
+++ 48th/somilee/LCS3.cpp
@@ -6,20 +6,17 @@ using namespace std;
 string str[3];
 int b[105][105][105];
 
-int m;
 int main(void) {
 	cin >> str[0] >> str[1] >> str[2];
 	for(int i = 0; i < str[0].size(); i++) {
 		for(int j = 0; j < str[1].size(); j++) {
-			if (str[0][i] == str[1][j]) {
-				for(int k = 0; k < str[2].size(); k++) {
-					if (str[0][i] == str[2][k]) {
-						b[i+1][j+1][k+1] = b[i][j][k] + 1;
-						m = max(m, b[i+1][j+1][k+1]);
-					}
-				}
+			for(int k = 0; k < str[2].size(); k++) {
+				if (str[0][i] == str[1][j] && str[1][j] == str[2][k])
+					b[i+1][j+1][k+1] = b[i][j][k] + 1;
+				else
+					b[i+1][j+1][k+1] = max(max(b[i][j+1][k+1], b[i+1][j][k+1]), b[i+1][j+1][k]);
 			}
 		}
 	}
-	cout << m << endl;
+	cout << b[str[0].size()][str[1].size()][str[2].size()] << endl;
 }
--- 48th/somilee/string_explosion.cpp
+++ 48th/somilee/string_explosion.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stack>
-#include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,11 +10,26 @@ stack<char> Q;
 int main(void) {
 	cin >> str >> e;
 	for (int i = 0; i < str.size(); i++) {
-		
+		Q.push(str[i]);
+		if (Q.top() == e[e.size()-1] && Q.size() >= e.size()) {
+			string c;
+			for(int j = 0; j < e.size(); j++) {
+				c.push_back(Q.top()); Q.pop();
+			}
+			reverse(c.begin(), c.end());
+			if (c != e)
+				for(int j = 0; j < c.size(); j++)
+					Q.push(c[j]);
+		}
+	}
+	if (Q.empty())
+		cout << "FRULA\n";
+	else {
+		string ans;
+		while(!Q.empty()) {
+			ans.push_back(Q.top()); Q.pop();
+		}
+		reverse(ans.begin(), ans.end());
+		cout << ans << endl;
 	}
 }
-
-/*
-12ab 1 12ab 2a 12 12ab ab b
-12ab
-*/
