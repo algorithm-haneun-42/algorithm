8ff28c1327fb72f51c70b0cb44e76df9c8f8c5ca
jeayoungKim529 <jeay529@gmail.com>
2024-08-05 16:59:30 +0900

    [FAIL] 25%

--- 53th/jeakim/robot_project.cpp
+++ 53th/jeakim/robot_project.cpp
@@ -6,39 +6,44 @@ using namespace std;
 
 int main()
 {
-	int x, n;
-	vector<int> v;
-
-	cin >> x >> n;
-	x *= 10000000;
-	for (int i = 0; i < n; i++)
+	while (42)
 	{
-		int l;
-
-		cin >> l;
-		v.push_back(l);
-	}
-	sort(v.begin(), v.end());
-
-	int st, en, flag;
-	st = 0;
-	en = n - 1;
-	flag = 0;
-	while (st != en)
-	{
-		int sum = v[st] + v[en];
-		if (sum == x)
-		{
-			cout << "yes " << v[st] << " " << v[en] << endl;
-			flag = 1;
+		if (cin.eof())
 			break ;
+		int x, n;
+		vector<int> v;
+
+		cin >> x >> n;
+		x *= 10000000;
+		for (int i = 0; i < n; i++)
+		{
+			int l;
+
+			cin >> l;
+			v.push_back(l);
 		}
-		else if (sum < x)
-			st++;
-		else if (sum > x)
-			en--; 
+		sort(v.begin(), v.end());
+
+		int st, en, flag;
+		st = 0;
+		en = n - 1;
+		flag = 0;
+		while (st < en)
+		{
+			int sum = v[st] + v[en];
+			if (sum == x)
+			{
+				cout << "yes " << v[st] << " " << v[en] << endl;
+				flag = 1;
+				break ;
+			}
+			else if (sum < x)
+				st++;
+			else if (sum > x)
+				en--; 
+		}
+		if (flag == 0)
+			cout << "danger" << endl;
 	}
-	if (flag == 0)
-		cout << "danger" << endl;
 	return (0);
 }
