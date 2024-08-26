f8329f47ce9ab79f5f010addcd3d8d8ebc76bf59
lamodadite <nmb11921@naver.com>
2024-08-26 20:46:54 +0900

    OrderOfHanoi

---
+++ 59th/jongmlee/OrderOfHanoi.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class OrderOfHanoi {
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sb.append((int)(Math.pow(2, n) - 1) + "\n");
+		hanoi(n, 1, 2, 3);
+		System.out.println(sb);
+	}
+	static void hanoi(int n, int from, int mid, int to) {
+		if (n == 1) {
+			sb.append(from + " " + to + "\n");
+			return;
+		}
+		hanoi(n - 1, from, to, mid);
+		sb.append(from + " " + to + "\n");
+		hanoi(n - 1, mid, from, to);
+	}
+}
