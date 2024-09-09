11f38e89ce2e5ee1bf53c2412645a7f8d83a3f11
jeayoungKim529 <jeay529@gmail.com>
2024-09-09 16:37:21 +0900

    [SCS]

--- 62th/jeakim/tictactoe.cpp
+++ 62th/jeakim/tictactoe.cpp
@@ -23,14 +23,14 @@ int check_x(string in)
 {
 	for (int i = 0; i < 3; i++)
 	{
-		if ((in[i * 3] == 'X') && (in[i * 3 + 1] == 'X') && (in[i * 3 + 2] == 'X'))
+		if ((in[i * 3] == 'X') && (in[i * 3 + 1] == 'X') && (in[i * 3 + 2] == 'X'))  //가로
 			return (1);
-		if ((in[i] == 'X') && (in[3 + i] == 'X') && (in[6 + i] == 'X'))
+		if ((in[i] == 'X') && (in[3 + i] == 'X') && (in[6 + i] == 'X')) //세로
 			return (1);
 	}
-	if ((in[2] == 'X') && (in[4] == 'X') && (in[5] == 'X'))
+	if ((in[2] == 'X') && (in[4] == 'X') && (in[6] == 'X')) //대각선
 		return (1);
-	if ((in[0] == 'X') && (in[4] == 'X') && (in[8] == 'X'))
+	if ((in[0] == 'X') && (in[4] == 'X') && (in[8] == 'X')) //대각선
 		return (1);
 	return (-1);
 }
@@ -44,9 +44,9 @@ int check_o(string in)
 		if ((in[i] == 'O') && (in[3 + i] == 'O') && (in[6 + i] == 'O')) //세로
 			return (0);
 	}
-	if ((in[0] == 'O') && (in[4] == 'O') && (in[8] == 'O'))
+	if ((in[0] == 'O') && (in[4] == 'O') && (in[8] == 'O')) //대각선
 		return (0);
-	if ((in[2] == 'O') && (in[4] == 'O') && (in[5] == 'O'))
+	if ((in[2] == 'O') && (in[4] == 'O') && (in[6] == 'O'))  //대각선
 		return (0);
 	return (-1);
 }
@@ -56,20 +56,8 @@ int	solution(string in)
 	pair<int, int> cnt;
 
 	cnt = count_num(in);
-	// if (abs(cnt.first - cnt.second) > 1)
-	// 	return (-1);
-	// if (cnt.first < 3)
-	// 	return (-1);
-	// if (cnt.first < cnt.second)
-	// 	return (-1);
-	// if (check_o(in) == 0 && (cnt.first > cnt.second))
-	// 	return (-1);
-	// if (check_x(in) == 1 && check_o(in) == 0)
-	// 	return (-1);
-	// if (check_x(in) == -1 && check_o(in) == 0 && ((cnt.first + cnt.second) == 9))
-	// 	return (-1);
-	// if (check_x(in) == -1 && check_o(in) == -1 && cnt.first < 5)
-	// 	return (-1);
+	if (abs(cnt.first - cnt.second) > 1)
+		return (-1);
 	if (check_x(in) == 1 && check_o(in) == -1 && (cnt.first == (cnt.second + 1)))
 		return (1);
 	if (check_x(in) == -1 && check_o(in) == 0 && (cnt.first == cnt.second))
