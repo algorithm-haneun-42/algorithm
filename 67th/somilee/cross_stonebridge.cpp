698a0f90449d6898c50d7fa7eb2e68253b0b7029
somilee <somilee@student.42seoul.kr>
2024-10-08 16:25:19 +0900

    ss

--- 67th/somilee/cross_stonebridge.cpp
+++ 67th/somilee/cross_stonebridge.cpp
@@ -3,21 +3,22 @@
 using namespace std;
 
 string d;
-string str[2]
-int b[21][2][101];
+string str[2];
+int b[2][21][101];
 int main(void) {
 	cin >> d >> str[0] >> str[1];
 	for(int i = 0; i < 101; i++) {
 		b[0][0][i] = 1;
-		b[0][1][i] = 1;
+		b[1][0][i] = 1;
 	}
-	for(int i = 0; i < 2; i++) {
-		for(int j = 1; j <= d.size(); j++) {
-			for(int k = 0; i < str[0].size(); k++) {
-				if(str[(i+j)%2][k] == d[j]) {
-					b[t ]
-				}
+	for(int i = 0; i < d.size(); i++) {
+		for(int j = 0; j < 2; j++) {
+			for(int k = 0; k < str[0].size(); k++) {
+				if(str[j][k] == d[i])
+					b[j][i+1][k+1] = b[(j+1)%2][i][k];
+				b[j][i+1][k+1] += b[j][i+1][k];
 			}
 		}
 	}
-}  ~
+	cout << b[0][d.size()][str[0].size()]+b[1][d.size()][str[0].size()] << endl;
+}
